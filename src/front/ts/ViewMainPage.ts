96b0725d3dc3832ed58a54f9014c2a54a3329174
Marcelo Castello <mcastello@MacBook-Pro-di-Marcelo-2012.local>
2020-10-07 19:47:06 -0300

    agregado de cambio de imagen en devices

--- src/front/ts/ViewMainPage.ts
+++ src/front/ts/ViewMainPage.ts
@@ -7,12 +7,19 @@ class ViewMainPage{
 
     showDevices(list: DeviceInt[]):void{
         let e:HTMLElement= this.myf.getElementById("devicesList");
+        let img:string;
+        //${dev.type=="1" ? "static/images/window.png":"static/images/lightbulb.png"}
         for (let dev of list){
-            
-
-            
+            switch(parseInt(dev.type)){
+                case 0:
+                    img="static/images/window.png";
+                    break;
+                case 1:
+                    img="static/images/lightbulb.png";
+                    break;
+            }
                 e.innerHTML+=`<li class="collection-item avatar">
-                <img src=${dev.type=="1" ? "static/images/window.png":"static/images/lightbulb.png"} alt="" class="circle">
+                <img src=${img} alt="" class="circle">
                 <span class="title">${dev.name}</span>
                 <p>${dev.description}<br>
                 <a href="#!" class="secondary-content">
