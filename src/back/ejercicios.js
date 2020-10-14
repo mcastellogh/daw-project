2ffcd045ec6e756ae97cf3a3f3a1cc1ea65a1098
mcastellogh <castello.marcelo@gmail.com>
2020-10-14 14:18:52 -0300

    hasta ejercicio 6 nodejs

---
+++ src/back/ejercicios.js
@@ -0,0 +1,34 @@
+//--Ejercitación
+var myjson = require('./datos.json');
+//console.log(myjson);
+
+var express = require('express');
+var app = express();
+app.use(express.json()); 
+
+//--Handlers
+function getHandler(req,res){
+    let id=req.query.id;
+    console.log(myjson[id-1]);
+    res.send(myjson[id-1]);
+}
+function postHandler(req,res){
+    let id=req.query.id;    //Obtengo la id que me postea el cliente
+    let st=req.query.st;    //Obtengo el estado que me postea el cliente
+    console.log(myjson[id-1],myjson[id-1].state);
+    myjson[id-1].state=st;  //cambio el valor de la clave 'state' del json y
+    res.send(myjson[id-1]); //la envío al cliente
+}
+
+//-- Recursos del server
+//--GET
+app.get('/',getHandler);
+
+//--POST
+app.post('/',postHandler);
+
+
+app.listen(3333,function(req,res){
+    console.log('Api!');
+})
+//--Para probar el GET: http://miot.mcastello.com:3333/?id=2
--- src/back/index.js
+++ src/back/index.js
@@ -14,6 +14,8 @@ var express = require('express');
 var app     = express();
 var mysql   = require('./mysql-connector');
 
+var myjson = require('./datos.json');
+
 // to parse application/json
 app.use(express.json()); 
 // to serve static files
@@ -22,9 +24,22 @@ app.use(express.static('/home/node/app/static/'));
 //=======[ Main module code ]==================================================
 
 app.get('/devices/', function(req, res, next) {
-    response = "{ 'key1':'value1' }"
-    res.send(JSON.stringify(response)).status(200);
+    let id=req.query.id;
+    console.log(myjson[id-1]);
+    res.send(myjson[id-1]);
+
+    //response = "{ 'key1':'value1' }"
+    //res.send(JSON.stringify(response)).status(200);
 });
+
+app.post ( '/', function(req,res){
+    let id=req.query.id;    //Obtengo la id que me postea el cliente
+    let st=req.query.st;    //Obtengo el estado que me postea el cliente
+    console.log(myjson[id-1],myjson[id-1].state);
+    myjson[id-1].state=st;  //cambio el valor de la clave 'state' del json y
+    res.send(myjson[id-1]); //la envío al cliente
+});
+
 
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
